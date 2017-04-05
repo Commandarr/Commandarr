f5c5223c55feea7ca856cc34497150188150e901
Todd Johnson <ToddAJohnson93@gmail.com>
2017-04-05 13:13:50 +1000

    Upload setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Comandarr',
+    version='0.1dev'.
+    packages=['Comandarr'],
+    license='Creative Commons',
+    long_description=open('README.md').read(),
+)
