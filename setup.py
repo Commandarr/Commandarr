43ff29b088eb1678aed7d86feadea6c9558559d9
Todd Johnson <ToddAJohnson93@gmail.com>
2017-04-05 13:33:03 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='Comandarr',
-    version='0.1dev'.
-    packages=['Comandarr'],
-    license='Creative Commons',
-    long_description=open('README.md').read(),
+  name='comandarr',
+  version='0.1'
+  packages=['comandarr'],
+  license='Creative Commons',
+  long_description=open('README.md').read(),
 )
+
+# To create a Python package that can be installed via pip run the following
+# commands in the terminal.
+# python setup.py resiter
+
+# REFERENCE: https://marthall.github.io/blog/how-to-package-a-python-app/
