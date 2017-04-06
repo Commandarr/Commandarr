6bb34c4860187ff743e3b6070cd635fa4b0ae8b4
Todd Johnson <toddajohnson93@gmail.com>
2017-04-06 19:05:54 +1000

    Add Code Comment Documentation

--- comandarr/sabnzdb.py
+++ comandarr/sabnzdb.py
@@ -1,1 +1,12 @@
-
+# #############################################################################
+# File Name: nzbget.py
+# Author: Todd Johnson
+# Creation Date: 06/04/2017
+#
+# Description: [To Be Completed]
+#
+# Offical website: https://www.comandarr.github.io
+# Github Source: https://www.github.com/comandarr/comandarr/
+# Readme Source: https://www.github.com/comandarr/comandarr/README.md
+#
+# #############################################################################
